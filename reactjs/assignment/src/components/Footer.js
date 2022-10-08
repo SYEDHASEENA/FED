072727f9e48fc3f350edf94d4dcc67080f169694
SYEDHASEENA <syedhaseena766@gmail.com>
2022-10-08 10:48:02 +0530

    initialCommit

---
+++ reactjs/assignment/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../myStyles.css";
+const Footer = () => {
+  return (
+    <div>
+      <div className="footer">
+        <div className="footer-content">
+            <h3>My FED Blog</h3>
+            <h2>Syed Haseena</h2>
+            <h2>20BQ1A05M6</h2>
+            <p>copyright &copy;2021 <a href=" #"> My-FED-Blog</a>  </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
